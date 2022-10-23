b511171ba789914b10fee2c9746564d1e52171ee
jiajiaanan <jiajiaanan@gmail.com>
2022-10-23 12:27:36 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -2,12 +2,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars') //載入handlebars
 const methodOverride = require('method-override')
-const session = require('express-session')
-const { proppatch } = require('./routes')
-// 載入設定檔，要寫在 express-session 以後
-const usePassport = require('./config/passport')
-
 const routes = require('./routes') // 引用路由器
+const session = require('express-session')
+const usePassport = require('./config/passport')// 載入設定檔，要寫在 express-session 以後
+
 require('./config/mongoose')
 
 const app = express()
@@ -16,22 +14,28 @@ const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) //指定副檔名
 app.set('view engine', 'hbs')
 
-// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
-usePassport(app)
-
-//使用body-parser
-app.use(express.urlencoded({ extended: true }))
-//使用method-override
-app.use(methodOverride('_method'))
-// 將 request 導入路由器
-app.use(routes)
-
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
 
+//使用body-parser
+app.use(express.urlencoded({ extended: true }))
+//使用method-override
+app.use(methodOverride('_method'))
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
+app.use((req, res, next) => {
+  console.log(req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+// 將 request 導入路由器
+app.use(routes)
 
 // 設定 port 3000
 app.listen(PORT, () => {
