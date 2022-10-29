5252c5cd2130d52655311b3ab338e3960da0a1a0
jiajiaanan <jiajiaanan@gmail.com>
2022-10-29 08:34:40 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const User = require('../../models/user')
 // 引用 passport
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 // login頁路由
 router.get('/login', (req, res) => {
@@ -53,18 +54,18 @@ router.post('/register', (req, res) => {
         password,
         confirmPassword
       })
-    } else {
-      // 如果還沒註冊：寫入資料庫
-      return User.create({
+    }
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash => User.create({
         name,
         email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
+        password: hash // 用雜湊值取代原本的使用者密碼
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   })
-    .catch(err => console.log(err))
 })
 
 //登出
